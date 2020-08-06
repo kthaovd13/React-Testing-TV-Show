7d9b6c84431aeece625a7a95b4292882116a957c
unknown <keng.thao@hotmail.com>
2020-08-05 22:13:58 -0500

    updated useEffect

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -3,8 +3,5 @@ import axios from 'axios'
 export const fetchShow = () => {
     return axios
       .get("https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes")
-      .then(res => {return res.data})
-      .catch(err => {
-          console.log("not fetching data", err.message)
-      })
-  };
+      .then(res => res)
+      }
